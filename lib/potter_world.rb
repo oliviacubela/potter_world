2cd1f70c4d84e3670171228df6817f77bd1b4ae9
Olivia Cubela <oliviacubela@gmail.com>
2020-08-13 12:28:10 -0500

    added methods to interact with user

--- lib/potter_world.rb
+++ lib/potter_world.rb
@@ -3,5 +3,3 @@ require_relative "./potter_world/version"
 require_relative "./potter_world/api.rb"
 require_relative "./potter_world/cli.rb"
 require_relative "./potter_world/potter_directory.rb"
-
-puts "environment file"
--- lib/potter_world/cli.rb
+++ lib/potter_world/cli.rb
@@ -1,7 +1,51 @@
 class PotterWorld::CLI
 
   def call
-    puts "i'm in the CLI"
+    puts "Hello User"
+    puts "To view the directory of wizards and withches, type 'view'"
+    puts "To exit the directory, type 'exit"
+    menu
   end
+
+  def menu 
+    input = gets.strip.downcase
+
+    if input == "view"
+      character_list 
+      menu 
+    elsif input == "exit"
+      goodbye 
+    else 
+      invalid_entry 
+    end
+  end
+
+  def character_list
+    puts "1. character 1"
+    puts "2. character 2"
+    puts "3. character 3"
+    puts ""
+    puts ""
+    puts "which wizard or witch would you like to know more about?"
+    input = gets.strip.downcase
+    
+    character_selection(input)
+  end
+
+  def character_selection(character)
+    #go over character array, create find method to find character selected
+
+  end
+
+  def goodbye
+    puts "Goodbye !"
+  end
+
+  def invalid_entry
+    puts "Invalid entry, try again"
+    menu
+  end
+
+
 
 end
