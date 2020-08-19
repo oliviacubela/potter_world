818a73239c705dd76d44e152718ef52fc91e4b7b
Olivia Cubela <oliviacubela@gmail.com>
2020-08-19 17:50:20 -0400

    refactored to loop

--- lib/potter_world/api.rb
+++ lib/potter_world/api.rb
@@ -1,14 +1,24 @@
 class API
+# require 'pry'
+# require 'json'
+# require 'rest-client'
 
   def self.get_char_data
+
       response = RestClient.get("https://www.potterapi.com/v1/characters?key=$2a$10$dzneyObJXfMmDwbSJS1LtONfqk526gSxHIl/ukaNki3OOSDwyw.JK")
       characters = JSON.parse(response.body)
-
+      # binding.pry
       characters.each do |character|
         PotterDirectory.new(character)
 
     end
   end
+
+
+  #Look at self.scrape_profile_page method in Scraper class. 
+  # maybe look at 'add_student_attributes' method in student scraper lab, in 'Student' class too...
+  #dynamically assigns the student attributes and values per the key/value pais of the hash using send method
+
 
   def self.get_spell_data
       response = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$dzneyObJXfMmDwbSJS1LtONfqk526gSxHIl/ukaNki3OOSDwyw.JK")
--- lib/potter_world/cli.rb
+++ lib/potter_world/cli.rb
@@ -1,34 +1,48 @@
 class PotterWorld::CLI
 
   def call
-    puts "Welcome!"
-    puts "To view the directory of wizards, witches, and creatures type 'view'"
-    puts "To view the comprehensive spellbook, type 'view spells'"
-    puts "To exit, type 'exit'"
-    API.get_char_data
+    input = ""
+
+    API.get_char_data  
     API.get_spell_data
-    menu
-  end
 
-  def menu 
-    # puts "To view the directory of wizards, witches, and creatures type 'view'"
-    # puts "To view the comprehensive spellbook, type 'view spells'"
-    # puts "To exit, type 'exit'"
-    input = gets.strip.downcase
+    while input != "exit"
+      puts "Welcome!"
+      puts "To view the directory of wizards, witches, and creatures type 'view'"
+      puts "To view the comprehensive spellbook, type 'view spells'"
+      puts "To exit, type 'exit'"
+      puts "What would you like to see first?"
 
-    if input == "exit"
-      goodbye
-    elsif input == "view"
-      character_list
-      menu
-    elsif input == "view spells"
-      spell_list
-      menu
-    else 
-      invalid_entry
-      menu
+      input = gets.strip.downcase
+
+      case input
+      when "view"
+        character_list
+      when "view spells"
+        spell_list
+      end
     end
   end
+
+  # def menu 
+  #   # puts "To view the directory of wizards, witches, and creatures type 'view'"
+  #   # puts "To view the comprehensive spellbook, type 'view spells'"
+  #   # puts "To exit, type 'exit'"
+  #   input = gets.strip.downcase
+
+  #   if input == "exit"
+  #     goodbye
+  #   elsif input == "view"
+  #     character_list
+  #     menu
+  #   elsif input == "view spells"
+  #     spell_list
+  #     menu
+  #   else 
+  #     invalid_entry
+  #     menu
+  #   end
+  # end
 
   def character_list
     PotterDirectory.all.each_with_index do |character, index|
@@ -50,7 +64,6 @@ class PotterWorld::CLI
       puts " House:".colorize(:red) + " #{c.house}".colorize(:yellow)
       puts " School:".colorize(:red) + " #{c.school}".colorize(:yellow)
       puts " Species:".colorize(:red) + " #{c.species}".colorize(:yellow)
-      menu
     end
   end
 
@@ -75,25 +88,25 @@ class PotterWorld::CLI
     end
   end
 
-  def goodbye
-    puts "Goodbye !"
-  end
+  # def goodbye
+  #   puts "Goodbye !"
+  # end
 
-  def invalid_entry
-    puts "Invalid entry - would you like to go back to the character directory, or the spellbook?"
-    puts "Type 'spell' for spellbook, or 'dir' for directory."
-    input = gets.strip.downcase
+  # def invalid_entry
+  #   puts "Invalid entry - would you like to go back to the character directory, or the spellbook?"
+  #   puts "Type 'spell' for spellbook, or 'dir' for directory."
+  #   input = gets.strip.downcase
 
-    if input == "spell"
-      spell_list 
-      menu 
-    elsif input == "dir"
-      character_list
-      menu
-    else 
-      goodbye 
-    end
-  end
+  #   if input == "spell"
+  #     spell_list 
+  #     menu 
+  #   elsif input == "dir"
+  #     character_list
+  #     menu
+  #   else 
+  #     goodbye 
+  #   end
+  # end
 
 
   #probably best to turn menu and invalid_entry methods into a loop, 
