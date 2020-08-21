a4d603e72f56ad095387fc5a4e903448a0e51c6c
Olivia Cubela <oliviacubela@gmail.com>
2020-08-21 13:14:18 -0400

    kept comments for reference

--- lib/potter_world/potter_directory.rb
+++ lib/potter_world/potter_directory.rb
@@ -5,7 +5,7 @@ class PotterDirectory
 
   def initialize(attr_hash)
     attr_hash.each do |k, v|
-      self.send(("#{k}="), v) if self.respond_to?(("#{k}="))
+      self.send(("#{k}="), v) if self.respond_to?(("#{k}=")) 
     end
     save
   end
@@ -25,4 +25,44 @@ class PotterDirectory
   end
 
 
+  # def add_character_attributes(attributes_hash)
+  #   attributes_hash.each do |attr, value|
+  #     self.send("#{attr}=", value)
+  #   end
+  #   self
+  # end
+
+
 end
+
+
+# class Student
+
+#   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+
+#   @@all = []
+
+#   def initialize(student_hash)
+#     student_hash.each do |attribute, value|
+#       self.send("#{attribute}=", value)
+#     end
+#     @@all << self
+#   end
+
+#   def self.create_from_collection(students_array)
+#     students_array.each do |student_hash|
+#       Student.new(student_hash)
+#     end
+#   end
+
+  # def add_student_attributes(attributes_hash)
+  #   attributes_hash.each do |attr, value|
+  #     self.send("#{attr}=", value)
+  #   end
+  #   self
+  # end
+
+#   def self.all
+#     @@all
+#   end
+# end
