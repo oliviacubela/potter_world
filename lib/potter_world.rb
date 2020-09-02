cb19dfec038112dda7c96ea1f7592e1bd53f9113
Olivia Cubela <oliviacubela@gmail.com>
2020-09-01 21:39:26 -0400

    renamed files & character class

--- lib/potter_world.rb
+++ lib/potter_world.rb
@@ -2,8 +2,8 @@ require_relative "./potter_world/version"
 
 require_relative "./potter_world/api.rb"
 require_relative "./potter_world/cli.rb"
-require_relative "./potter_world/potter_directory.rb"
-require_relative "./potter_world/spell_directory.rb"
+require_relative "./potter_world/character.rb"
+require_relative "./potter_world/spell.rb"
 
 require 'rest-client'
 require 'json'
--- lib/potter_world/api.rb
+++ lib/potter_world/api.rb
@@ -5,7 +5,7 @@ class API
       response = RestClient.get("https://www.potterapi.com/v1/characters?key=$2a$10$dzneyObJXfMmDwbSJS1LtONfqk526gSxHIl/ukaNki3OOSDwyw.JK")
       characters = JSON.parse(response.body)
       characters.each do |character|
-        PotterDirectory.new(character)
+        CharacterDirectory.new(character)
 
     end
   end
